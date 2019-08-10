24026b811adf0094bb36b132800b8a5638af5d06
Maurycy Moczulski <mmoczulski@o2.pl>
2019-08-10 17:11:15 +0200

    ostatni

--- java.scipt/zadankopizza/java.js
+++ java.scipt/zadankopizza/java.js
@@ -1,67 +1,58 @@
-const order = {
-    imie: "",
-    nazwisko: "",
-    pizza: "",
-    sosy: [],
-    zgoda: Boolean
-}
 
-let przycisk = $('#a');
-    
-    
         $('#a').on("click", function()
         {    
-    let zgoda = $('[wybor]'[0].checked);
-    let imie = $('[imie]'[0].val());
-    let nazwisko = $('[nazwisko]'[0].val());
-    let ulica = $('[ulica]'[0].value)
-    let nrdomu = $('[nrdomu]'[0].value)
-    let kod = $('[kod]'[0].value)
-    let miasto = $('[miasto]'[0].value)
-    let sospo = $('[sospo]'[0].checked)
-    let soscz = $('[soscz]'[0].checked)
-    let pizza = $('#pizza'[0].value)
+    let zgoda = $('#rodo');
+    let imie = $('#imie');
+    let nazwisko = $('#nazwisko');
+    let ulica = $('#ulica')
+    let nrdomu = $('#nrdomu')
+    let kod = $('#kod')
+    let miasto = $('#miasto')
+    let sospo = $('#sospo')
+    let soscz = $('#soscz')
+    let pizza = $('#pizza')
+    let price = $('#price')
     console.log(imie)
-        if (zgoda == false){ 
-        e.preventDefault();
+        if (zgoda.is(":checked") == false){ 
          alert( 'zaznacz zgode na rodo!' );}
         
      else {
-        if (imie == ""){
+        if (imie.val() == ""){
             $( "#div1" ).html( "<span>nie wpisałeś imienia!</span>!" );
             }
         else {
             
-            if (nazwisko == ""){
+            if (nazwisko.val() == ""){
                 $( "#div2" ).html( "<span>nie wpisałeś nazwiska!</span>!" );  
             } 
            else {
            
-            if (ulica == ""){
+            if (ulica.val() == ""){
                 $( "#div3" ).html( "<span>nie wpisałeś ulicy!</span>!" );
             }
                 else {
                  
-            if (nrdomu == "") {
+            if (nrdomu.val() == "") {
                 $( "#div4" ).html( "<span>nie wpisałeś numeru domu/mieszkania!</span>!" );
             } 
                 else {
-            if (kod == "") {
+            if (kod.val() == "") {
                 $( "#div5" ).html( "<span>nie wpisałeś kodu pocztowego!</span>!" );
             }
                 else {
-            if (miasto == "") {
+            if (miasto.val() == "") {
                 $( "#div6" ).html( "<span>nie wpisałeś miasta!</span>!" );
             }   
                 else {
-            if (sospo == false && soscz == false) {
+            if (sospo.is(":checked") == false && soscz.is(":checked") == false) {
                 $( "#div7" ).html( "<span>nie wybrales sosu!</span>!" );
 
             }
             else {
-            if (pizza == "") {
+            if (pizza.val() == "-- wybierz pizze --") {
                 $("#pizza").html("<span>nie wybrales pizzy!</span>")
             }
+            else{}
             }
                 }    
                 }
@@ -71,6 +62,66 @@ let przycisk = $('#a');
            } 
         }
       }
+
+
+let priceTable = [
+    {
+    id:0,
+    price:45
+    },
+    {
+    id:1,
+    price:34
+    },
+    {
+    id:2,
+    price:46
+    },
+    {
+    id:3,
+    price:53
+    },
+    {
+    id:4,
+    price:43
+    }
+];
+
+pizza.change(function(){
+    let pizzaPrice = 0;
+    let pizzaType = $(this).val();
+    console.log("wybrana pizza",pizzaType)
+
+    if (pizzaType == 11) {
+     price.text("0 PLN");
+     return;
+    }
+
+    priceTable.forEach(function(element) {
+    if (element.id == pizzaType) {
+        pizzaPrice = element.price;
+
+        price.text(pizzaPrice + "PLN")
+
+        return false;
+    }
+    let orderObject = {
+        imie: $.trim(imie.val()),
+        nazwisko: $.trim(nazwisko.val()),
+        ulica: $.trim(ulica.val()),
+        nrdomu: $.trim(nrdomu.val()),
+        kod: $.trim(kod.val()),
+        miasto: $.trim(miasto.val()),
+        pizza: pizza.val(),
+        sosy: [sospo.is(":checked"), soscz.is(":checked")]
+    };
+
+    
+   
+
+     return false;
+    });
+
+    console.log(orderObject);
 });
-
-
+});
